ab9a964f50970f665d6d2fd82eb04a1e79ae1d56
Adam Rasheed <adamrasheed91@gmail.com>
2017-12-18 12:04:16 +0400

    added lazyloading

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 const burger = document.getElementById('burger');
 const mobMenu = document.querySelector('.mobile-menu');
 
+
 // left: 37, up: 38, right: 39, down: 40,
 // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
 var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
-
 function preventDefault(e) {
     e = e || window.event;
     if (e.preventDefault)
         e.preventDefault();
     e.returnValue = false;
 }
-
 function preventDefaultForScrollKeys(e) {
     if (keys[e.keyCode]) {
         preventDefault(e);
