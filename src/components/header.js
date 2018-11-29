7ed5d1af180a2114911c810d666621ac2b5ca801
adamrasheed <adamrasheed91@gmail.com>
2018-11-29 08:59:06 -0800

    added mobile menu

--- src/components/header.js
+++ src/components/header.js
@@ -2,30 +2,30 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Navigation from './Navigation'
-import MobileNav from './MobileNav';
+import MobileNav from './MobileNav'
 
 const Header = ({ siteTitle, jobTitle }) => {
-
-  const handleClick = ({currentTarget : self}) => {
-    self.classList.toggle('burger--open');
-  };
+  const handleClick = ({ currentTarget: self }) => {
+    const mobileMenu = document.querySelector('[data-mobile-menu]')
+    self.classList.toggle('burger--open')
+    mobileMenu.classList.toggle('mobile-menu--open')
+  }
 
   return (
     <header className="header">
       <div className="container header__container">
         <Link to="/" className="header__logo logo" id="logo">
-          <h1 className="logo__title">{ siteTitle }</h1>
-          <div className="logo__desc">{ jobTitle }</div>
+          <h1 className="logo__title">{siteTitle}</h1>
+          <div className="logo__desc">{jobTitle}</div>
         </Link>
         <div id="burger" className="burger" onClick={handleClick}>
           <span className="burger__span burger__span-1" />
           <span className="burger__span burger__span-2" />
           <span className="burger__span burger__span-3" />
         </div>
-  
-        <Navigation/>
-        <MobileNav/>
-  
+
+        <Navigation />
+        <MobileNav />
       </div>
     </header>
   )
