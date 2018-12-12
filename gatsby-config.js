44fcf41fd56ac200e82ee70669502e2a8f454e93
adamrasheed <adamrasheed91@gmail.com>
2018-12-12 10:19:51 -0800

    updated meta data and added sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,15 +4,9 @@ module.exports = {
     jobTitle: `Front-end Developer`,
     siteUrl: `https://adamrasheed.com`,
     tagLine: `I’m a Front-end Developer based in <br>San Diego, CA 🏖.`,
-    social: [
-      { account: 'twitter', url: `https://twitter.com/ARasheedPhoto` },
-      { account: 'instagram', url: `https://www.instagram.com/hashtagadam/` },
-      { account: 'github', url: `https://github.com/adamrasheed` },
-      { account: 'dribbble', url: `https://dribbble.com/adamrasheed` },
-      { account: 'youtube', url: `http://bit.ly/SketchVids` },
-    ],
   },
   plugins: [
+    `gatsby-plugin-sitemap`,
     'gatsby-plugin-react-helmet',
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`,
@@ -28,18 +22,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Adam Rasheed',
+        short_name: 'Adam Rasheed',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#ffffff',
+        theme_color: '#000000',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/adam-rasheed-icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
     {
       resolve: `gatsby-source-wordpress`,
       options: {
