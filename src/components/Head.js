4102252956885440f4a4fecbf0f4121709644ea1
adamrasheed <adamrasheed91@gmail.com>
2018-12-12 10:09:23 -0800

    updated helmet meta data

---
+++ src/components/Head.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+const fallbackMeta = {
+  title: 'Adam Rasheed | San Diego Front-end Developer',
+}
+
+class Head extends React.Component {
+  render() {
+    const { title, description, tagline, photo } = this.props
+    return (
+      <Helmet>
+        <html lang="en" />
+        <title>{title ? title : fallbackMeta.title}</title>
+        <meta
+          name="description"
+          content={
+            description
+              ? description
+              : 'Adam Rasheed is a front-end developer based in San Diego, California who specializes in custom Shopify theme development and custom WordPress theme development'
+          }
+        />
+
+        <meta
+          name="keywords"
+          content="front-end developer, san diego react developer, San Diego Shopify developer"
+        />
+
+        <meta
+          property="og:title"
+          content={title ? title : fallbackMeta.title}
+        />
+        {/*<!-- Twitter -->*/}
+        <meta name="twitter:card" content="summary_large_image" />
+        {description ? (
+          <meta name="twitter:card" content={description} />
+        ) : null}
+
+        <meta
+          name="twitter:title"
+          content={
+            title ? title : 'Adam Rasheed | San Diego Front-end Developer'
+          }
+        />
+        <meta name="twitter:site" content="@arasheedphoto" />
+        <meta name="twitter:creator" content="@arasheedphoto" />
+        <meta
+          name="twitter:description"
+          content={
+            description
+              ? description
+              : 'Adam Rasheed is a front-end developer based in San Diego, California who specializes in custom Shopify theme development and custom WordPress theme development'
+          }
+        />
+        {photo ? <meta name="twitter:image:src" content={photo} /> : null}
+
+        {/*<!-- Open Graph general (Facebook, Pinterest & Google+) -->*/}
+        <meta name="og:title" content={title ? title : fallbackMeta.title} />
+        {photo ? <meta name="og:image" content={photo} /> : null}
+
+        <meta name="og:url" content="https://adamrasheed.com" />
+        <meta name="og:site_name" content="Adam Rasheed" />
+        <meta name="og:type" content="website" />
+      </Helmet>
+    )
+  }
+}
+
+export default Head
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
+import Head from './Head'
 import Header from './Global/Header/Header'
 import Footer from './Footer/Footer'
 
@@ -17,6 +17,7 @@ const Layout = ({ children, template }) => (
       query SiteTitleQuery {
         wordpressSiteMetadata {
           name
+          description
         }
         wordpressAcfOptions {
           options {
@@ -45,61 +46,15 @@ const Layout = ({ children, template }) => (
     `}
     render={data => (
       <>
-        <Helmet
+        <Head
           title={data.wordpressSiteMetadata.name}
-          meta={[
-            {
-              name: 'description',
-              content:
-                'Adam Rasheed is a front-end developer based in San Diego, California who specializes in custom Shopify theme development and custom WordPress theme development',
-            },
-            {
-              name: 'keywords',
-              content: 'front-end developer, shopify developer',
-            },
-          ]}
-        >
-          <html lang="en" />
-          <meta
-            property="og:title"
-            content="Adam Rasheed | San Diego Front-end Developer"
-          />
-          {/*<!-- Twitter -->*/}
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta
-            name="twitter:card"
-            content={data.wordpressAcfOptions.options.tagline}
-          />
-          <meta
-            name="twitter:title"
-            content={data.wordpressSiteMetadata.name}
-          />
-          <meta name="twitter:site" content="@arasheedphoto" />
-          <meta name="twitter:creator" content="@arasheedphoto" />
-          <meta
-            name="twitter:description"
-            content={data.wordpressAcfOptions.options.tagline}
-          />
-          <meta
-            name="twitter:image:src"
-            content={
-              data.wordpressPage.featured_media.localFile.childImageSharp.fixed
-                .src
-            }
-          />
-          {/*<!-- Open Graph general (Facebook, Pinterest & Google+) -->*/}
-          <meta name="og:title" content="m" />
-          <meta
-            name="og:image"
-            content={
-              data.wordpressPage.featured_media.localFile.childImageSharp.fixed
-                .src
-            }
-          />
-          <meta name="og:url" content="https://adamrasheed.com" />
-          <meta name="og:site_name" content="Adam Rasheed" />
-          <meta name="og:type" content="website" />
-        </Helmet>
+          description={data.wordpressSiteMetadata.description}
+          tagline={data.wordpressAcfOptions.options.tagline}
+          photo={
+            data.wordpressPage.featured_media.localFile.childImageSharp.fixed
+              .src
+          }
+        />
         <Header
           siteTitle={data.wordpressSiteMetadata.name}
           jobTitle={data.wordpressAcfOptions.options.title}
